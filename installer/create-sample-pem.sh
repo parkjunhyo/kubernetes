5930d20f192d4e6d4d69355a0315fc1c43081ab8
junhyo <junhyo.luck@gmail.com>
2019-05-20 03:39:33 +0000

    update

--- installer/create-sample-pem.sh
+++ installer/create-sample-pem.sh
@@ -6,3 +6,25 @@ cat << EOF > ./.sample.pem
 -----END RSA PRIVATE KEY-----
 EOF
 chmod 400 ./.sample.pem
+
+# add env
+MYACCOUNT=$(id | awk -F"[()]" '{print $2}')
+MYCONF='/'$MYACCOUNT'/.bashrc'
+CUR_DIR=$(pwd)'/usr/bin'
+
+RESULT=$(cat $MYCONF | grep -i 'path')
+if [[ $RESULT ]]
+then
+        PATHRESULT=$(cat $MYCONF | grep -i $CUR_DIR)
+        if [[ ! $PATHRESULT ]]
+        then
+                VALUES=$(echo $RESULT | awk -F"[=]" '{print $2}')
+                REP_STRING=$VALUES':'$CUR_DIR
+                sed -i 's/^PATH/# PATH/' $MYCONF
+                echo "" >> $MYCONF
+                echo $REP_STRING >> $MYCONF
+        fi
+else
+        echo "" >> $MYCONF
+        echo "PATH="\$PATH:"$CUR_DIR" >> $MYCONF
+fi
