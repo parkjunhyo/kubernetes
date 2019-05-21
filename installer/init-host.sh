d4a9a0ef141f7201fad1bb4d2ab48273a9b4f81e
junhyo <junhyo.luck@gmail.com>
2019-05-21 04:47:15 +0000

    revise

--- installer/init-host.sh
+++ installer/init-host.sh
@@ -8,7 +8,8 @@ echo "" >> /etc/hosts
 for i in "${!IPMS[@]}"; do
  echo ${IPMS[$i]}" "${NNS[$i]} >> /etc/hosts
 done
-FIP=$(ifconfig bond0 | grep -i "inet addr:" | awk -F"[:]" '{print $2}' | awk -F"[ ]" '{print $1;}')
+
+FIP=$(ifconfig $MAINIF | grep -i "inet addr:" | awk -F"[:]" '{print $2}' | awk -F"[ ]" '{print $1;}')
 HN=$(cat /etc/hosts | grep -i $FIP | awk -F"[ ]" '{print $2;}')
 hostname $HN
 echo $HN > /etc/hostname
